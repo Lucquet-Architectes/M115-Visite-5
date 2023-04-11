a7faf7708021dbfc318c2cfd06130e094126c1e5
hyvenn <56298319+hyvenn@users.noreply.github.com>
2023-04-11 17:51:45 +0200

    M115 Visite 5

---
+++ data.js
@@ -0,0 +1,264 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-m115-r5-espace-accueil",
+      "name": "M115 R+5 espace accueil",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3750,
+      "initialViewParameters": {
+        "yaw": 0.060276081947534976,
+        "pitch": 0.048102453068414874,
+        "fov": 1.41569238083293
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.29999010576737994,
+          "pitch": -0.027426750442719694,
+          "rotation": 9.42477796076938,
+          "target": "3-m115-r5-espace-bureau"
+        },
+        {
+          "yaw": 0.8766609665279042,
+          "pitch": -0.43571227858047834,
+          "rotation": 4.71238898038469,
+          "target": "1-m115-r6-espace-bureau"
+        },
+        {
+          "yaw": 0.877085894718352,
+          "pitch": -0.3561247867056956,
+          "rotation": 1.5707963267948966,
+          "target": "2-m115-salle-de-runion"
+        },
+        {
+          "yaw": 1.1315370438081054,
+          "pitch": -0.0103548978535688,
+          "rotation": 3.141592653589793,
+          "target": "4-m115-r5-espace-repro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-m115-r6-espace-bureau",
+      "name": "M115 R+6 espace bureau",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.023210710852055,
+          "pitch": 0.13745294973539934,
+          "rotation": 2.356194490192345,
+          "target": "0-m115-r5-espace-accueil"
+        },
+        {
+          "yaw": 2.106429504597463,
+          "pitch": 0.05282522786788846,
+          "rotation": 1.5707963267948966,
+          "target": "2-m115-salle-de-runion"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-m115-salle-de-runion",
+      "name": "M115 salle de réunion",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3000,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5541397772495049,
+          "pitch": 0.001308030468678112,
+          "rotation": 4.71238898038469,
+          "target": "1-m115-r6-espace-bureau"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-m115-r5-espace-bureau",
+      "name": "M115 R+5 espace bureau",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8335108419611572,
+          "pitch": -0.02130607903419346,
+          "rotation": 3.141592653589793,
+          "target": "0-m115-r5-espace-accueil"
+        },
+        {
+          "yaw": -0.09995368166799068,
+          "pitch": -0.017710488466970986,
+          "rotation": 3.141592653589793,
+          "target": "4-m115-r5-espace-repro"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-m115-r5-espace-repro",
+      "name": "M115 R+5 espace repro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 3750,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4427382206759294,
+          "pitch": -0.017605297333048497,
+          "rotation": 2.356194490192345,
+          "target": "0-m115-r5-espace-accueil"
+        },
+        {
+          "yaw": 1.2048776539213222,
+          "pitch": 0.0011617765671090297,
+          "rotation": 3.9269908169872414,
+          "target": "3-m115-r5-espace-bureau"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1200615119360169,
+          "pitch": 0.007321380348496831,
+          "title": "zone à travailler",
+          "text": "<br>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
